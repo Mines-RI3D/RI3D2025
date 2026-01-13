3f1ccc2e3844a48e27f055b3b8df178dd681986b
bakedpotatolord <jeffy9745@gmail.com>
2026-01-13 15:22:17 -0700

    add climber subsystem

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -1,11 +1,37 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimberConstants;
 
 public class Climber extends SubsystemBase {
     
 
+    SparkMax motor = new SparkMax(ClimberConstants.motorID,MotorType.kBrushless);
+
     public Climber(){
+        motor.configure(ClimberConstants.motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
         
+        setDefaultCommand(Commands.run(this::stop));
     }
+
+    public void runForwards(){
+        motor.set(1);
+    }
+
+    public void stop(){
+        motor.stopMotor();
+    }
+
+    public void runReverse(){
+        motor.set(-1);
+    }
+
+    
+
 }
